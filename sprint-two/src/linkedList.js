af384d18660c837263457617401af58971791ce6
Strom415 <mattstrom415@gmail.com>
2018-04-26 16:11:32 -0700

    made linkedList tests to pass

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -1,15 +1,39 @@
 var LinkedList = function() {
   var list = {};
-  list.head = null;
-  list.tail = null;
-
-  list.addToTail = function(value) {
+  list.head = null; //always first item in list
+  list.tail = null; //always the last item in list
+  
+  list.addToTail = function(value) {  
+    var newNode = Node(value);
+    if (list.head === null) {
+      list.head = newNode;
+      list.tail = newNode;
+    } else {
+      //check on how to set next value/make sure we aren't overwriting first next
+      list.tail.next = newNode;
+      list.tail = list.tail.next;
+    }
   };
 
   list.removeHead = function() {
+    var returnVal = list.head.value;
+    list.head = list.head.next;
+    return returnVal;
   };
 
   list.contains = function(target) {
+    //check list for target input
+    var current = list.head;
+    // if (current.value === target) {
+    //   return true;
+    // }
+    while (current) {
+      if (current.value === target) {
+        return true;
+      }
+      current = current.next;
+    }
+    return false;
   };
 
   return list;
