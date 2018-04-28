2a28bc5348b06fbcc5dc1b639e48142da30d4834
Strom415 <mattstrom415@gmail.com>
2018-04-28 16:02:51 -0700

    implemented binarySearchTree and passed all tests

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,7 +1,42 @@
 var BinarySearchTree = function(value) {
+  var tree = Object.create(BinarySearchTree.prototype);
+  tree.value = value;
+  tree.left = undefined;
+  tree.right = undefined;
+  return tree;
 };
 
+BinarySearchTree.prototype.insert = function(value, current = this) {
+  if (value < current.value && current.left === undefined) {
+    current.left = BinarySearchTree(value);
+  } else if (value < current.value && current.left !== undefined) {
+    current.left.insert(value);
+  } else if (value > current.value && current.right === undefined) {
+    current.right = BinarySearchTree(value);
+  } else if (value > current.value && current.right !== undefined) {
+    current.right.insert(value);
+  }
+}
 
-/*
- * Complexity: What is the time complexity of the above functions?
- */
+BinarySearchTree.prototype.contains = function(value, current = this) {
+  if (current.value === value) {
+    return true;
+  }
+  if (value < current.value && current.left !== undefined) {
+    return current.left.contains(value);
+  }
+  if (value > current.value && current.right !== undefined) {
+    return current.right.contains(value);
+  }
+  return false;
+}
+
+BinarySearchTree.prototype.depthFirstLog = function(func, current = this) {
+  func(current.value);
+  if (current.left !== undefined) {
+    current.left.depthFirstLog(func);
+  }
+  if (current.right !== undefined) {
+    current.right.depthFirstLog(func);
+  }
+}
